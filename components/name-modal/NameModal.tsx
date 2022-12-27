d7959030441fe7512b26b86c0cf8a72977b5fee3
jmurphy5613 <jmurphy5613@gmail.com>
2022-12-26 20:21:05 -0500

    did styles for hover button to exit modal

--- components/name-modal/NameModal.tsx
+++ components/name-modal/NameModal.tsx
@@ -1,13 +1,17 @@
 import styles from './NameModal.module.css'
+import {BsXLg} from 'react-icons/bs'
 
 const NameModal = () => {
     return (
         <div className={styles.container}>
             <div className={styles["modal-container"]}>
-                <div className={styles.exit}></div>
+                <div className={styles.exit}>
+                    <div className={styles.background}></div>
+                    <BsXLg className={styles.x} />
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default NameModal
