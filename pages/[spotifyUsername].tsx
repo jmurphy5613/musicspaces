2741002aea27a46292086a952d39cb6d36fea98c
jmurphy5613 <jmurphy5613@gmail.com>
2023-05-09 13:30:26 -0400

    set up count up

--- pages/[spotifyUsername].tsx
+++ pages/[spotifyUsername].tsx
@@ -1,8 +1,29 @@
 import styles from '../styles/Profile.module.css'
 import Navbar from '../components/navbar/Navbar'
 import Image from 'next/image'
+import { useRef, useEffect } from 'react'
+import { CountUp } from 'countup.js'
+
 
 const Profile = () => {
+
+    const countupRef = useRef('0')
+    let countup: CountUp
+
+    useEffect(() => {
+        initCountUps()
+    }, [])
+
+    async function initCountUps() {
+        const countUpModule = await import('countup.js');
+        countup = new countUpModule.CountUp(countupRef.current, 128)
+        if (!countup.error) {
+          countup.start()
+        } else {
+          console.error(countup.error)
+        }
+    }
+
     return (
         <>
             <Navbar />
@@ -43,11 +64,11 @@ const Profile = () => {
                     </div>
                     <div className={styles["user-facts"]}>
                         <div className={styles.fact}>
-                            <h1 className={styles.figure}>64</h1>
+                            <h1 ref={countupRef as unknown as React.RefObject<HTMLDivElement>} className={styles.figure}></h1>
                             <h3 className={styles["figure-description"]}>unique songs</h3>
                         </div>
                         <div className={styles.fact}>
-                            <h1 className={styles.figure}>128</h1>
+                            <h1 className={styles.figure}></h1>
                             <h3 className={styles["figure-description"]}>hours listened</h3>
                         </div>
                     </div>
