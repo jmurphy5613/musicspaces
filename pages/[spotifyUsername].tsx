a56569de63834d208586687032aef7eb106c5b83
jmurphy5613 <jmurphy5613@gmail.com>
2023-05-09 11:06:55 -0400

    sync

--- pages/[spotifyUsername].tsx
+++ pages/[spotifyUsername].tsx
@@ -1,5 +1,6 @@
 import styles from '../styles/Profile.module.css'
 import Navbar from '../components/navbar/Navbar'
+import Image from 'next/image'
 
 const Profile = () => {
     return (
@@ -8,7 +9,25 @@ const Profile = () => {
             <div className={styles.container}>
                 <div className={styles["user-container"]}>
                     <div className={styles["user-info-container"]}>
-
+                        <div className={styles["image-container"]}>
+                            <Image 
+                                src="/johntransparent.png"
+                                alt='profile picture'
+                                fill
+                                style={{borderRadius: '100%'}}
+                            />
+                        </div>
+                        <div className={styles["user-description"]}>
+                            <div style={{ display: 'flex', alignItems: 'flex-end', width: '50%' }}>
+                                <h1 className={styles.name}>John Murphy</h1>
+                                <h3 className={styles.username}>@jmurphy5613</h3>
+                            </div>
+                            <div style={{ display: 'flex' }}>
+                                <h3>6 followers</h3>
+                                <h3></h3>
+                                <h3>6 following</h3>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
