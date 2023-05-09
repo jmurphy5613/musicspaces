13c80d1011c1047bbcc166c0cc8ca93f8995fc1f
jmurphy5613 <jmurphy5613@gmail.com>
2023-05-09 13:36:11 -0400

    done preliminary box

--- pages/[spotifyUsername].tsx
+++ pages/[spotifyUsername].tsx
@@ -2,25 +2,39 @@ import styles from '../styles/Profile.module.css'
 import Navbar from '../components/navbar/Navbar'
 import Image from 'next/image'
 import { useRef, useEffect } from 'react'
-import { CountUp } from 'countup.js'
+import { CountUp, CountUpOptions } from 'countup.js'
 
 
 const Profile = () => {
 
-    const countupRef = useRef('0')
+    const countupRef1 = useRef('0')
+    const countupRef2 = useRef('0')
+
     let countup: CountUp
+    let countup2: CountUp
 
     useEffect(() => {
         initCountUps()
     }, [])
 
+    //create options variable for countup
+    const options:CountUpOptions = {
+        duration: 2,
+        useEasing: true,
+        useGrouping: true,
+        
+    }
+
+
     async function initCountUps() {
         const countUpModule = await import('countup.js');
-        countup = new countUpModule.CountUp(countupRef.current, 128)
-        if (!countup.error) {
-          countup.start()
+        countup = new countUpModule.CountUp(countupRef1.current, 128, options)
+        countup2 = new countUpModule.CountUp(countupRef2.current, 325, options)
+        if (!countup.error && !countup2.error) {
+            countup.start()
+            countup2.start()    
         } else {
-          console.error(countup.error)
+            console.error(countup.error)
         }
     }
 
@@ -64,11 +78,11 @@ const Profile = () => {
                     </div>
                     <div className={styles["user-facts"]}>
                         <div className={styles.fact}>
-                            <h1 ref={countupRef as unknown as React.RefObject<HTMLDivElement>} className={styles.figure}></h1>
+                            <h1 ref={countupRef1 as unknown as React.RefObject<HTMLDivElement>} className={styles.figure}></h1>
                             <h3 className={styles["figure-description"]}>unique songs</h3>
                         </div>
                         <div className={styles.fact}>
-                            <h1 className={styles.figure}></h1>
+                            <h1 ref={countupRef2 as unknown as React.RefObject<HTMLDivElement>} className={styles.figure}></h1>
                             <h3 className={styles["figure-description"]}>hours listened</h3>
                         </div>
                     </div>
