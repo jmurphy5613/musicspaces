cbdeb33d30f67696a3139c5661a9a217009b7e39
jmurphy5613 <jmurphy5613@gmail.com>
2023-05-10 11:43:55 -0400

    got multiple tabs to work

--- components/profile-tabs/ProfileTabs.tsx
+++ components/profile-tabs/ProfileTabs.tsx
@@ -3,20 +3,41 @@ import styles from './ProfileTabs.module.css'
 
 const ProfileTabs = () => {
 
+    const tabs = [
+        {
+            label: 'Top Songs',
+            icon: '/sound_light.svg'
+        },
+        {
+            label: 'Top Artists',
+            icon: '/mic_light.svg'
+        },
+        {
+            label: 'Recent Songs',
+            icon: '/alarmclock_light.svg'
+        }
+    ]
+
 
     return (
         <div className={styles.navigation}>
-            <div className={styles["navigation-item"]}>
-                <div className={styles["navigation-image-container"]}>
-                    <Image
-                        src='/sound_light.svg'
-                        fill
-                        alt='sound icon'
-                    />
-                </div>
-                <h2 className={styles["navigation-label"]}>Top Songs</h2>
-            </div>
-            <div className={styles.overlay} />
+            {tabs.map((tab, index) => {
+                return (
+                    <>
+                        <div className={styles["navigation-item"]}>
+                            <div className={styles["navigation-image-container"]}>
+                                <Image
+                                    src={tab.icon}
+                                    fill
+                                    alt='icon'
+                                />
+                            </div>
+                            <h2 className={styles["navigation-label"]}>{tab.label}</h2>
+                            <div className={styles.overlay} />
+                        </div>
+                    </>
+                )
+            })}
         </div>
     )
 }
