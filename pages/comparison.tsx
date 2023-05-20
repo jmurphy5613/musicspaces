cb7b1b9e01b05e66160a15ce844b220f832900ea
jmurphy5613 <jmurphy5613@gmail.com>
2023-05-20 17:45:48 -0400

    grid component done

--- pages/comparison.tsx
+++ pages/comparison.tsx
@@ -3,6 +3,7 @@ import Navbar from '../components/navbar/Navbar'
 import ComparisonBlock from '../components/comparison-block/ComparisonBlock'
 import ComparisonGrid from '../components/comparison-grid/ComparisonGrid'
 import { useEffect } from 'react'
+import { topArtists } from '../utils/data'
 
 const Comparison = () => {
 
@@ -15,7 +16,7 @@ const Comparison = () => {
             <Navbar />
             <div className={styles.container}>
                 <ComparisonBlock />
-
+                <ComparisonGrid gridItems={topArtists.slice(0, 5)} title="Common Artists" />
             </div>
 
         </>
