4c9accf34c8e0aa01ce8b1a11882955e6f916f2f
jmurphy5613 <jmurphy5613@gmail.com>
2023-05-30 11:10:18 -0400

    adjusting for prod

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,7 +53,7 @@ export default function Home() {
 
     return (
         <>
-            <UsernamePopup />
+            {/* <UsernamePopup /> */}
             <Navbar />
             <div className={styles.container}>
                 <div className={styles["background-images"]}>
