e00b6cdbfb7170360aedcbb4adc6536371a27cb9
jmurphy5613 <jmurphy5613@gmail.com>
2023-05-30 11:08:35 -0400

    frontend for username selection

--- components/username-popup/UsernamePopup.tsx
+++ components/username-popup/UsernamePopup.tsx
@@ -1,5 +1,6 @@
 import styles from './UsernamePopup.module.css'
 import { useState } from 'react'
+import Image from 'next/image'
 
 const UsernamePopup = () => {
 
@@ -11,9 +12,15 @@ const UsernamePopup = () => {
                 <h1 className={styles.title}>enter your <span className={styles.purple}>musicspaces</span>  name</h1>
                 <input onChange={e => setUsernameEntered(e.target.value)} className={styles["username-input"]} />
                 <h2 className={styles["link-title"]}>your custom link: <span className={styles.purple}>{`musicspaces.com/${usernameEntered}`}</span></h2>
-                <button>
-                    <h2>continue</h2>
-                    
+                <button className={styles["continue-button"]}>
+                    <h2 className={styles["button-label"]}>continue</h2>
+                    <div className={styles["icon-container"]}>
+                        <Image 
+                            src="/icons/Expand_right.svg"
+                            alt="arrow-right"
+                            fill
+                        />
+                    </div>
                 </button>
             </div>
         </div>
