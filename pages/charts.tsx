925a2456cc3e0e45460ef93563711be241acd255
jmurphy5613 <jmurphy5613@gmail.com>
2023-07-10 14:33:27 -0400

    set up async chart

--- pages/charts.tsx
+++ pages/charts.tsx
@@ -1,64 +1,99 @@
-import styles from '../styles/Charts.module.css'
-import Navbar from '../components/navbar/Navbar'
-import { useEffect, useRef } from 'react'
-
-
-import Chart, { ChartItem } from 'chart.js/auto';
+import styles from '../styles/Charts.module.css';
+import Navbar from '../components/navbar/Navbar';
+import { useEffect, useRef } from 'react';
+import Chart, { ChartConfiguration, ChartItem } from 'chart.js/auto';
 import { TreemapController, TreemapElement } from 'chartjs-chart-treemap';
+import { getRecentlyPlayed, getTopSongs } from '../utils/requests/userData';
 
 Chart.register(TreemapController, TreemapElement);
 
-const Charts = () => {
+const data = [
+    { category: 'main', value: 1 },
+    { category: 'main', value: 2 },
+    { category: 'main', value: 3 },
+    { category: 'other', value: 4 },
+    { category: 'other', value: 5 },
+];
 
-    const canvas = useRef<HTMLCanvasElement>(null)
+const Charts = () => {
+    const canvas = useRef<HTMLCanvasElement>(null);
+    const chartRef = useRef<Chart | null>(null); // Ref to store the chart instance
 
     useEffect(() => {
-        if (!canvas.current) return
-        const ctx = canvas.current.getContext('2d');
+        if (!canvas.current) return;
 
-        function colorFromRaw(ctx, border) {
-            if (ctx.type !== 'data') {
-                return 'transparent';
-            }
-            const value = ctx.raw.v;
-            let alpha = (1 + Math.log(value)) / 5;
-            const color = '255, 255, 255';
-            if (border) {
-                alpha += 0.5;
-            }
-            return 'rgba(' + color + ', ' + alpha + ')'
-        }
+        const setUpMap = async () => {
+            const ctx = canvas.current.getContext('2d');
 
-        const config = {
-            type: 'treemap',
-            data: {
-                datasets: [
-                    {
-                        label: 'My First dataset',
-                        tree: [1, 2, 3, 4, 5],
-                        borderColor: (ctx) => colorFromRaw(ctx, true),
-                        borderWidth: 1,
-                        spacing: -0.5, // Animations look better when overlapping a little
-                        backgroundColor: (ctx) => colorFromRaw(ctx, true),
-                    }
-                ],
-            },
+            function colorFromRaw(ctx: any, border: boolean) {
+                if (ctx.type !== 'data') {
+                    return 'transparent';
+                }
+                const value = ctx.raw.v;
+                let alpha = value / 9;
+                console.log(alpha);
+                const color = '167, 178, 255';
+                return 'rgba(' + color + ', ' + alpha + ')';
+            }
+
+            const config: ChartConfiguration = {
+                type: 'treemap',
+                data: {
+                    datasets: [
+                        {
+                            label: 'My First dataset',
+                            tree: data,
+                            key: 'value',
+                            groups: ['category'],
+                            borderColor: 'rgba(167, 178, 255)',
+                            // borderWidth: 1,
+                            spacing: -0.5, // Animations look better when overlapping a little
+                            backgroundColor: (ctx: CanvasRenderingContext2D) => colorFromRaw(ctx, true),
+                        },
+                    ],
+                },
+                options: {
+                    maintainAspectRatio: false,
+                    plugins: {
+                        legend: {
+                            display: false,
+                        },
+                        tooltip: {
+                            displayColors: false,
+                        },
+                    },
+                },
+            };
+
+            if (chartRef.current) {
+                chartRef.current.destroy();
+            }
+
+            chartRef.current = new Chart(ctx, config); // Store the chart instance
+
+            // No need to return a cleanup function here
+
+            // Clean up the chart instance on component unmount
+            return () => {
+                if (chartRef.current) {
+                    chartRef.current.destroy();
+                }
+            };
         };
 
-        const chart = new Chart(ctx, config);
-        return () => chart.destroy();
-
-    }, [])
+        setUpMap()
+    }, []);
 
     return (
         <>
             <Navbar />
             <div className={styles.container}>
-                <canvas className={styles.treemap} ref={canvas}></canvas>
+                <div className={styles['treemap-container']}>
+                    <canvas ref={canvas}></canvas>
+                </div>
             </div>
         </>
+    );
+};
 
-    )
-}
-
-export default Charts
+export default Charts;
